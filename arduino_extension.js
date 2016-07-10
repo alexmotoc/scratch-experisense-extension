2c5234be65617eac22c802d8edfb6fd5560c9329
Kieran Hall <kieranh5511@gmail.com>
2016-07-10 23:17:44 +0100

    Logs :(

--- arduino_extension.js
+++ arduino_extension.js
@@ -267,10 +267,12 @@
               break;
             case ANALOG_MESSAGE:
               setAnalogInput(multiByteChannel, (storedInputData[0] << 7) + storedInputData[1]);
-              console.log(analogReadCallbacks);
+              //console.log(analogReadCallbacks);
               while (analogReadCallbacks[multiByteChannel] !== undefined && 
                   analogReadCallbacks[multiByteChannel].length > 0) {
                 //Call all callback functions with new data
+                console.log('function called');
+                console.log(analogReadCallbacks);
                 analogReadCallbacks[multiByteChannel].pop()((storedInputData[0] << 7) + storedInputData[1]);
               }
               break;
