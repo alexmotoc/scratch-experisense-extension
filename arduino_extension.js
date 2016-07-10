f41edd95305e23aff26fa7293056f93f555eced5
Kieran Hall <kieranh5511@gmail.com>
2016-07-10 22:58:03 +0100

    More debug :(

--- arduino_extension.js
+++ arduino_extension.js
@@ -219,7 +219,8 @@
         for (var i = 1; i < sysexBytesRead; i++)
           analogChannel[i-1] = storedInputData[i];
           //initialise callback queue for analog pin number
-          analogReadCallbacks[storedInputData[i]] = [];
+          console.log('pushing callback ' + (storedInputData[i] - 1));
+          analogReadCallbacks[storedInputData[i] - 1] = [];
         for (var pin = 0; pin < analogChannel.length; pin++) {
           if (analogChannel[pin] != 127) {
             var out = new Uint8Array([
