11dbc445bcd39f5a1931cbc49f1f26b75a25167a
Kieran Hall <kieranh5511@gmail.com>
2017-06-28 10:59:47 +0100

    Start work on adapting to ExperiSense

--- newexperisense.js
+++ newexperisense.js
@@ -10,24 +10,13 @@
 
     // Sensor states:
     var channels = {
-        slider: 7,
-        light: 5,
-        sound: 6,
-        button: 3,
-        'resistance-A': 4,
-        'resistance-B': 2,
-        'resistance-C': 1,
-        'resistance-D': 0
-    };
-    var inputs = {
-        slider: 0,
-        light: 0,
-        sound: 0,
-        button: 0,
-        'resistance-A': 0,
-        'resistance-B': 0,
-        'resistance-C': 0,
-        'resistance-D': 0
+        EXT2: {channel: 0, value: null, sensitivity: false},
+        EXT1: {channel: 1, value: null, sensitivity: false},
+        RES_D: {channel: 2, value: null, sensitivity: false},
+        RES_C: {channel: 3, value: null, sensitivity: false},
+        RES_B: {channel: 4, value: null, sensitivity: false},
+        RES_A: {channel: 5, value: null, sensitivity: false},
+        button: {channel: 6, value: null}
     };
 
     ext.resetAll = function(){};
@@ -47,73 +36,65 @@
         return getSensorPressed(which);
     };
 
-    ext.sensor = function(which) { return getSensor(which); };
+    ext.sensor = function(which) {
+        return getSensor(which);
+    };
 
     // Private logic
     function getSensorPressed(which) {
         if (device == null) return false;
         if (which == 'button pressed' && getSensor('button') < 1) return true;
-        if (which == 'A connected' && getSensor('resistance-A') < 10) return true;
-        if (which == 'B connected' && getSensor('resistance-B') < 10) return true;
-        if (which == 'C connected' && getSensor('resistance-C') < 10) return true;
-        if (which == 'D connected' && getSensor('resistance-D') < 10) return true;
+        if (which == 'A connected' && getSensor('RES_A') < 10) return true;
+        if (which == 'B connected' && getSensor('RES_B') < 10) return true;
+        if (which == 'C connected' && getSensor('RES_C') < 10) return true;
+        if (which == 'D connected' && getSensor('RES_D') < 10) return true;
         return false;
     }
 
     function getSensor(which) {
-        return inputs[which];
+        if (which === 'light sensor') {
+            return getLightSensor();
+        } else if (which === 'dial') {
+            return channels.EXT1.value;
+        }
+    }
+    
+    function getLightSensor() {
+        var v = channels.EXT2.value;
+        return (v < 25) ? 100 - v : Math.round((1023 - v) * (75 / 998));
     }
 
-    var inputArray = [];
     function processData() {
         var bytes = new Uint8Array(rawData);
 
-        inputArray[15] = 0;
+        inputArray[7] = 0;
 
         // TODO: make this robust against misaligned packets.
         // Right now there's no guarantee that our 18 bytes start at the beginning of a message.
         // Maybe we should treat the data as a stream of 2-byte packets instead of 18-byte packets.
         // That way we could just check the high bit of each byte to verify that we're aligned.
-        for(var i=0; i<9; ++i) {
-            var hb = bytes[i*2] & 127;
-            var channel = hb >> 3;
-            var lb = bytes[i*2+1] & 127;
-            inputArray[channel] = ((hb & 7) << 7) + lb;
+        for (var i = 0; i < 9; ++i) {
+            var hb = bytes[i*2] & 0x7F;
+            var channel = (hb >> 3) & 0x07;
+            var lb = bytes[i*2+1] & 0x7F;
+            channels[channel].sensitivity = !!(hb >> 6);
+            channels[channel].value = ((hb & 0x07) << 7) + lb;
         }
 
-        if (watchdog && (inputArray[15] == 0x04)) {
+        if (watchdog && (inputArray[15] == 0x01)) {
             // Seems to be a valid PicoBoard.
             clearTimeout(watchdog);
             watchdog = null;
-        }
-
-        for(var name in inputs) {
-            var v = inputArray[channels[name]];
-            if(name == 'light') {
-                v = (v < 25) ? 100 - v : Math.round((1023 - v) * (75 / 998));
-            }
-            else if(name == 'sound') {
-                //empirically tested noise sensor floor
-                v = Math.max(0, v - 18)
-                v =  (v < 50) ? v / 2 :
-                    //noise ceiling
-                    25 + Math.min(75, Math.round((v - 50) * (75 / 580)));
-            }
-            else {
-                v = (100 * v) / 1023;
-            }
-
-            inputs[name] = v;
         }
 
         //console.log(inputs);
         rawData = null;
     }
 
-    function appendBuffer( buffer1, buffer2 ) {
-        var tmp = new Uint8Array( buffer1.byteLength + buffer2.byteLength );
-        tmp.set( new Uint8Array( buffer1 ), 0 );
-        tmp.set( new Uint8Array( buffer2 ), buffer1.byteLength );
+    function appendBuffer(buffer1, buffer2) {
+        var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+        tmp.set(new Uint8Array(buffer1), 0);
+        tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
         return tmp.buffer;
     }
 
@@ -135,7 +116,7 @@
         device = potentialDevices.shift();
         if (!device) return;
 
-        device.open({ stopBits: 0, bitRate: 38400, ctsFlowControl: 0 });
+        device.open({stopBits: 0, bitRate: 38400, ctsFlowControl: 0});
         device.set_receive_handler(function(data) {
             //console.log('Received: ' + data.byteLength);
             if(!rawData || rawData.byteLength == 18) rawData = new Uint8Array(data);
@@ -150,7 +131,7 @@
 
         // Tell the PicoBoard to send a input data every 50ms
         var pingCmd = new Uint8Array(1);
-        pingCmd[0] = 1;
+        pingCmd[0] = 0x02;
         poller = setInterval(function() {
             device.send(pingCmd.buffer);
         }, 50);
@@ -179,9 +160,9 @@
     };
 
     ext._getStatus = function() {
-        if(!device) return {status: 1, msg: 'PicoBoard disconnected'};
-        if(watchdog) return {status: 1, msg: 'Probing for PicoBoard'};
-        return {status: 2, msg: 'PicoBoard connected'};
+        if(!device) return {status: 1, msg: 'ExperiSense disconnected'};
+        if(watchdog) return {status: 1, msg: 'Probing for ExperiSense'};
+        return {status: 2, msg: 'ExperiSense connected'};
     }
 
     var descriptor = {
@@ -196,7 +177,8 @@
             sensor: ['slider', 'light', 'sound', 'resistance-A', 'resistance-B', 'resistance-C', 'resistance-D'],
             lessMore: ['>', '<']
         },
+        //TODO: update URL
         url: '/info/help/studio/tips/ext/PicoBoard/'
     };
-    ScratchExtensions.register('PicoBoard', descriptor, ext, {type: 'serial'});
+    ScratchExtensions.register('ExperiSense', descriptor, ext, {type: 'serial'});
 })({});
