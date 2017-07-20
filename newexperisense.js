1eb69823eb6e7bd608a2e21bb02c7031ef8427c8
Kieran Hall <kieranh5511@gmail.com>
2017-07-20 11:38:07 +0200

    ADD %m.

--- newexperisense.js
+++ newexperisense.js
@@ -326,7 +326,7 @@
     var descriptor = {
         blocks: [
             ['h', 'when %m.booleanSensor', 'whenSensorConnected', 'button pressed'],
-            ['h', 'when resistanceAndSensor %m.lessMore %n', 'whenSensorPass', 'dial', '>', 50],
+            ['h', 'when %m.resistanceAndSensor %m.lessMore %n', 'whenSensorPass', 'dial', '>', 50],
             ['b', 'sensor %m.booleanSensor?', 'sensorPressed', 'button pressed'],
             ['-'],
             ['r', '%m.sensor sensor value', 'sensor', 'dial'],
